fd50869f533cfc9932edaad726f4cea0cc9feaf3
Phil <natsudragoneel282003@gmail.com>
2025-09-10 22:49:50 -0700

    having trouble adding google font vida to tailwind css; will come back to work on it tomorrow

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./html_pages/*.html",
+    "./*.html",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        viga: ['Viga', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
