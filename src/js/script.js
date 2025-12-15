5b2940051d15162668fb0b5b309dcf5be3a723da
Phil <natsudragoneel282003@gmail.com>
2025-12-14 20:17:28 -0800

    Quick evaluation of Formspree AJAX backend processing

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,47 @@
 // The below code is for handling the contact form submisson via Formspree using AJAX
 
-const form = document.querySelector('#contact-form');
-const statusMessage = document.querySelector('#form-status');
+const form = document.querySelector('#contact-form')
+const statusMessage = document.querySelector('#form-status')
 
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault(); // stops the normal page redirect to Formspree
+//   // adding an event listener to the form's submit event
+//   // (see how the query selector on line 3 selects the whole form itself by its id)
 
-    statusMessage.textContent = "Sending...";
-    statusMessage.className = "mt-4 text-sm text-blue-600";
+form.addEventListener('submit', async (event) => {
+  event.preventDefault(); // stops the normal page redirect to Formspree
 
-    const formData = new FormData(form);
+  statusMessage.textContent = "Sending..."
+  statusMessage.className = "my-4 text-sm text-blue-600" // adds margin top-bottom with text properties
 
-    try {
-      const response = await fetch(form.action, {
-        method: form.method, // POST
-        body: formData, // form data itself
-        headers: { 'Accept': 'application/json' } // expects the response in JSON format
-      });
+  const formData = new FormData(form) // extracts the form's data and puts it as an object (key-value pairs)
 
-      if (response.ok) {
+  try {
+    const response = await fetch(form.action, {
+      method: form.method, // POST
+      body: formData, // form data itself
+      headers: { Accept: 'application/json' } // expects the response in JSON format'}
+    })
+
+    if (response.ok) {
         // SUCCESS
         statusMessage.textContent = "Thank you! Your message has been sent.";
-        statusMessage.className = "mt-4 text-sm text-green-600";
+        statusMessage.className = "mt-4 text-md text-green-600";
         form.reset();
-      } else {
+      }
+      else {
         // ERROR from Formspree
         const result = await response.json();
+
         if (result.errors) {
           statusMessage.textContent = result.errors.map(e => e.message).join(", ");
-        } else {
+        }
+        else {
           statusMessage.textContent = "Oops! Something went wrong. Please try again.";
         }
-        statusMessage.className = "mt-4 text-sm text-red-600";
+        statusMessage.className = "mt-4 text-md text-red-600";
       }
     } catch (error) {
-      // NETWORK ERRORso
+      // NETWORK ERROR
       statusMessage.textContent = "Network error. Please try again later.";
-      statusMessage.className = "mt-4 text-sm text-red-600";
+      statusMessage.className = "mt-4 text-md text-red-600";
     }
 });
