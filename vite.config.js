ddd880c734e5700ece48505536ebd59f72fbf989
Phil <natsudragoneel282003@gmail.com>
2026-02-13 13:29:33 -0800

    Added other pages in Vite build for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
 export default defineConfig({
-  base: "/portfolio/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        projects: resolve(__dirname, "projects.html"),
+        resume: resolve(__dirname, "resume.html"),
+        contact: resolve(__dirname, "contact.html"),
+        // misc: resolve(__dirname, "misc.html"), // (optional)
+      },
+    },
+  },
 });
